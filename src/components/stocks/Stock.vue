26027b146fbee911c3c060b52f972bd34f1a7d00
Simas <simonas@digitouch.lt>
2019-04-25 10:08:48 +0300

    dynamic stock rendering, purchase quantity validation

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,22 +3,27 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panle-title">
-          Name
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
           <input
             type="number"
-            name=""
-            id=""
             class="form-control"
             placeholder="Quantity"
+            v-model.number="quantity"
           />
         </div>
         <div class="pull-right">
-          <button class="btn btn-success">Buy</button>
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          >
+            Buy
+          </button>
         </div>
       </div>
     </div>
@@ -26,6 +31,23 @@
 </template>
 <script>
 export default {
-  name: "StockVue"
+  name: "StockVue",
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
 };
 </script>
